0053e46c897c32f0ef175a6a7163357a70a47465
Nathan Walters <nwalters512@gmail.com>
2016-09-05 19:28:56 -0500

    Added rest of fields to dummy webcast

--- react/gameday2/components/__tests__/EmbedTwitch.snapshot.js
+++ react/gameday2/components/__tests__/EmbedTwitch.snapshot.js
@@ -5,6 +5,11 @@ import EmbedTwitch from '../EmbedTwitch'
 describe('EmbedTwitch snapshot', () => {
   it('renders correctly', () => {
     const webcast = {
+      key: '2016test',
+      num: 0,
+      id: '2016test-0',
+      name: 'Test Event',
+      type: 'twitch',
       channel: 'test',
     }
 
