1551d71e26e2d01d85d8dff103c584f12441c48c
Nathan Walters <nwalters512@gmail.com>
2016-09-22 11:34:26 -0500

    Added custom theme for Mui

--- react/gameday2/gameday2.js
+++ react/gameday2/gameday2.js
@@ -2,6 +2,8 @@ import 'babel-polyfill'
 import React from 'react'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import { indigo500, indigo700 } from 'material-ui/styles/colors'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { createStore, applyMiddleware, compose } from 'redux'
@@ -22,9 +24,17 @@ const store = createStore(gamedayReducer, compose(
   window.devToolsExtension ? window.devToolsExtension() : f => f
 ))
 
+// Set up theme
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: indigo500,
+    primary2Color: indigo700,
+  },
+})
+
 ReactDOM.render(
   <Provider store={store}>
-    <MuiThemeProvider>
+    <MuiThemeProvider muiTheme={muiTheme}>
       <GamedayFrame />
     </MuiThemeProvider>
   </Provider>,
